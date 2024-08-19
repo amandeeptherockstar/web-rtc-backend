5102ba11ef5a7620004c055ea91542b956c6d32e
Amandeep Kochhar <amandeeptherockstar@gmail.com>
2024-08-19 11:25:07 +0530

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+// server/index.js
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST"], // Allow GET and POST methods
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("offer", (offer) => {
+    socket.broadcast.emit("offer", offer);
+  });
+
+  socket.on("answer", (answer) => {
+    socket.broadcast.emit("answer", answer);
+  });
+
+  socket.on("ice-candidate", (candidate) => {
+    socket.broadcast.emit("ice-candidate", candidate);
+  });
+});
+
+server.listen(2000, () => {
+  console.log("listening on *:2000");
+});
